e797e0589e208c8c4d749d203d1c10a026138ce8
NickVanVliet <Nicholas.VanVliet@southeasttech.edu>
2026-04-12 21:05:18 -0500

    updated server ro see pages and get specific html pages

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-// app.js
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Home Page");
+    res.sendFile(__dirname + '/pages/index.html');
 });
-
+app.get('/new', (req, res) => {
+    res.sendFile(__dirname + '/pages/newCat.html');
+});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
