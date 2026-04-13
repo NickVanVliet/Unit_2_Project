0246ac9535131c926adde4a0a1d172a1f0f922aa
NickVanVliet <Nicholas.VanVliet@southeasttech.edu>
2026-04-12 23:06:58 -0500

    changed server to dynamically show homepage from .json

--- server.js
+++ server.js
@@ -8,11 +8,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/index.html');
+    const cats = catEdit.readCats();
+
+    let html = "<h1>Cat List</h1><a href='/new'>Add Cat</a><ul>";
+
+  if (cats.length === 0) {
+        html += `<li>No cats yet</li>`;
+    }
+    cats.forEach(cat => {
+        html += `
+            <li>
+                ${cat.name} (${cat.age}, ${cat.breed})
+                <a href="/delete/${cat.name}">Delete</a>
+            </li>
+        `;
+    });
+
+    html += "</ul>";
+
+    res.send(html);
 });
+
 app.get('/new', (req, res) => {
     res.sendFile(__dirname + '/pages/newCat.html');
 });
+
 app.post('/new', (req, res) => {
     const newCat = {
         name: req.body.name,
